e4e3f6a54c7f07cc9bb0baab7ba1c208841210ef
mubaraq <paynedaze@gmail.com>
2024-02-15 11:57:04 +0100

    ch: pay text updated

--- src/Components/MusicPay.jsx
+++ src/Components/MusicPay.jsx
@@ -214,7 +214,7 @@ const MusicPay = () => {
                 description: 'Payment for music ',
                 logo: 'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg',
               },
-              text: 'Link To Pay ',
+              text: 'Click To Pay ',
               callback: handlePaymentSuccess,
               onClose: handlePaymentClose,
             }}
