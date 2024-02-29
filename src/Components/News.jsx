9d495f596ce1a71999517df08af435d67cad3f6a
mubaraq <paynedaze@gmail.com>
2024-02-29 22:54:41 +0100

    ch: date changed

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -9,7 +9,7 @@ const News = () => {
             <div className=" text-[white] p-2  text-center md:hidden block absolute top-28 right-2 drop-shadow-[4px]">
                 <small className="font-bold text-xs">Pre-order {"Hericoni's"} new single </small>
                 <p className="text-xl font-bold my-2 bg-[black] rounded w-20 mx-auto">Aje ale</p>
-                <small className="font-bold text-xs me-1">featuring Sheliroy<br/>  (dropping on first March 2024) by</small>
+                <small className="font-bold text-xs me-1">featuring Sheliroy<br/>  (dropping on 1st March 2024) by</small>
                 <a href="https://distrokid.com/hyperfollow/hericoni/aje-ale-feat-sheliroy-2" target="_blank" rel="noreferrer" className="hover:text-oxBlood text-[9px] block bg-[black] rounded px-1 py-1 w-20 mx-auto mt-2" >clicking here</a>
             </div>
             <img src={Artist} className="h-[400px] rounded-s-2xl rounded-e-2xl " alt="Artist image"/>
@@ -17,7 +17,7 @@ const News = () => {
         <div className=" text-[white] p-2 md:text-start text-center md:block hidden">
             <small className="font-bold text-base">Pre-order {"Hericoni's"} new single </small>
             <p className="text-4xl font-bold my-2">Aje ale</p>
-            <small className="font-bold text-sm">featuring Sheliroy (dropping on first March 2024) by </small>
+            <small className="font-bold text-sm">featuring Sheliroy (dropping on 1st March 2024) by </small>
             <a href="https://distrokid.com/hyperfollow/hericoni/aje-ale-feat-sheliroy-2" target="_blank" rel="noreferrer" className="hover:text-oxBlood text-sm" >clicking here</a>
         </div>
 
